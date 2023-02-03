caf90c61d91c1e7e10d331c425590af33c039662
Kycb42148 <kycb42148@gmail.com>
2023-02-03 06:33:53 +0200

    HUD and players HashMap

--- src/main/java/net/voiddustry/redvsblue/RedVsBluePlugin.java
+++ src/main/java/net/voiddustry/redvsblue/RedVsBluePlugin.java
@@ -1,7 +1,28 @@
 package net.voiddustry.redvsblue;
 
+import arc.Events;
+import mindustry.game.EventType;
+import mindustry.gen.Call;
+import mindustry.gen.Unit;
+import mindustry.gen.Groups;
 import mindustry.mod.Plugin;
 
-public class RedVsBluePlugin extends Plugin {
+import java.util.HashMap;
 
+public class RedVsBluePlugin extends Plugin {
+    private final HashMap<String, PlayerData> players = new HashMap<>();
+
+    public void init() {
+        Events.on(EventType.PlayerConnect.class, event -> {
+            if (!players.containsKey(event.player.uuid()))
+                players.put(event.player.uuid(), new PlayerData(event.player));
+        });
+
+        Events.run(EventType.Trigger.update, () -> Groups.player.each(player -> {
+            Unit unit = player.unit();
+            PlayerData data = players.get(player.uuid());
+
+            Call.setHudText(player.con(), Bundle.format("game.hud", Math.floor(unit.health()), Math.floor(unit.shield()), data.getScore()));
+        }));
+    }
 }
