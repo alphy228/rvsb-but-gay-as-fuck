955d37bb1cd205bbecef81f014765203f2603124
alphy228 <89208763+alphy228@users.noreply.github.com>
2025-08-06 17:40:32 +0300

    fix fucking stupid quad server crash

--- src/main/java/net/voiddustry/redvsblue/game/units/special/Quad.java
+++ src/main/java/net/voiddustry/redvsblue/game/units/special/Quad.java
@@ -15,32 +15,34 @@ public class Quad {
     public static void initAbilities() {
         Groups.unit.forEach(unit -> {
             if(unit.type == UnitTypes.quad && unit.team == Team.blue) {
-                int centerX = unit.tileOn().x * 8;
-                int centerY = unit.tileOn().y * 8;
+                if (unit.x < Vars.world.width && unit.x > 0 && unit.y < Vars.world.height && unit.y > 0) {
+                    int centerX = unit.tileOn().x * 8;
+                    int centerY = unit.tileOn().y * 8;
 
-                for (int i = 0; i < 40; i++) {
-                    Call.effect(Fx.healWaveDynamic, (float) (centerX + Math.sin(i) * 64), (float) (centerY + Math.cos(i) * 64), 1, Color.red);
-                }
-                Groups.unit.forEach(u -> {
-                    if(u.team == Team.crux && u.dst(unit.x, unit.y) <= 64) {
-                        Call.effect(Fx.fire, u.x, u.y, 2, Color.red);
+                    for (int i = 0; i < 40; i++) {
+                        Call.effect(Fx.healWaveDynamic, (float) (centerX + Math.sin(i) * 64), (float) (centerY + Math.cos(i) * 64), 1, Color.red);
+                    }
+                    Groups.unit.forEach(u -> {
+                        if(u.team == Team.crux && u.dst(unit.x, unit.y) <= 64) {
+                            Call.effect(Fx.fire, u.x, u.y, 2, Color.red);
 
-                        u.apply(StatusEffects.shocked, 120);
+                            u.apply(StatusEffects.shocked, 120);
 
-                        float damage = u.maxHealth/200;
+                            float damage = u.maxHealth/200;
 
-                        if (u.health >= damage) {
-                            u.health = u.health() - damage;
-                            Call.label("[#55557F]-" + damage, 0.5f, u.x, u.y);
-                        } else {
-                            u.kill();
-                            if(unit.getPlayer() != null) {
-                                PlayerData data = players.get(unit.getPlayer().uuid());
-                                data.addScore(data.getLevel());
-                                data.addExp(1);
+                            if (u.health >= damage) {
+                                u.health = u.health() - damage;
+                                Call.label("[#55557F]-" + damage, 0.5f, u.x, u.y);
+                            } else {
+                                u.kill();
+                                if(unit.getPlayer() != null) {
+                                    PlayerData data = players.get(unit.getPlayer().uuid());
+                                    data.addScore(data.getLevel());
+                                    data.addExp(1);
+                                }
+
+                                Call.label("[scarlet]-" + damage, 3f, u.x, u.y);
                             }
-
-                            Call.label("[scarlet]-" + damage, 3f, u.x, u.y);
                         }
                     }
                 });
