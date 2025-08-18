88174db52c6051ca312d1d82519701987796d73e
alphy228 <89208763+alphy228@users.noreply.github.com>
2025-08-18 10:17:52 +0300

    Update MapVote.java

--- src/main/java/net/voiddustry/redvsblue/util/MapVote.java
+++ src/main/java/net/voiddustry/redvsblue/util/MapVote.java
@@ -89,7 +89,7 @@ public class MapVote {
 
     public static void registerVote(Player player, Integer voteNumber) {
         if (!playersVotesMap.containsKey(player.uuid())) {
-            if (avaibleMapnumbers.contains(voteNumber-1)) {
+            if (avaibleMapnumbers.contains(voteNumber-1) || getMaps().get(voteNumber-1).file.name().startsWith("[blue]")) {
                 playersVotesMap.put(player.uuid(), voteNumber-1);
             }
         }
