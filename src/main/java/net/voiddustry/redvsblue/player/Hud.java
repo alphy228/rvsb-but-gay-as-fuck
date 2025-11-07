283c5ae86b4c3518592402e8ded5a08914b5e563
alphy228 <89208763+alphy228@users.noreply.github.com>
2025-11-07 13:29:15 +0300

    Update Hud.java

--- src/main/java/net/voiddustry/redvsblue/player/Hud.java
+++ src/main/java/net/voiddustry/redvsblue/player/Hud.java
@@ -65,29 +65,37 @@ public class Hud {
 
         Groups.player.each(player -> {
             Unit unit = player.unit();
+           
             PlayerData data = RedVsBluePlugin.players.get(player.uuid());
-            if (!(unit == null)) {
     
-                if(!RedVsBluePlugin.players.containsKey(player.uuid())) {
-                    RedVsBluePlugin.players.put(player.uuid(), new PlayerData(player));
-                }
+            if(!RedVsBluePlugin.players.containsKey(player.uuid())) {
+                RedVsBluePlugin.players.put(player.uuid(), new PlayerData(player));
+            }
     
-                String textHud = (data.getLevel() == 5)? "[scarlet]Max" : "[accent]" + data.getExp() + " / " + data.getMaxExp();
-    
+            String textHud = (data.getLevel() == 5)? "[scarlet]Max" : "[accent]" + data.getExp() + " / " + data.getMaxExp();
+  
+
+            if (player.unit() == null) {
                 String hudText = Bundle.format("game.hud", Bundle.findLocale(player.locale()), Administration.Config.serverName.get(), Math.floor(unit.health()), Math.floor(unit.shield()), data.getScore(), RedVsBluePlugin.stage, time, playersText, data.getLevel(), textHud);
-                Call.setHudText(player.con, hudText);
+            } else {
+                String hudText = Bundle.format("game.hud", Bundle.findLocale(player.locale()), Administration.Config.serverName.get(), "-", "-", data.getScore(), RedVsBluePlugin.stage, time, playersText, data.getLevel(), textHud);
+            }
+
+            Call.setHudText(player.con, hudText);
     
-                if (RedVsBluePlugin.playing && data.getUnit() != null) {
-                    if (data.getUnit().dead) {
-                        data.setTeam(Team.crux);
-                        player.team(data.getTeam());
-                    }
+            if (RedVsBluePlugin.playing && data.getUnit() != null) {
+                if (data.getUnit().dead) {
+                    data.setTeam(Team.crux);
+                    player.team(data.getTeam());
                 }
-    
+            }
+
+            if (!(player.unit() == null)) {
                 if (player.unit().type == UnitTypes.quasar && player.unit().shield >= -10 && player.unit().shield <= 0) {
                     player.unit().shield = 300;
                 }
             }
+            
             if (RedVsBluePlugin.playing && data.getUnit() != null && player.team() == Team.blue) {
                 if (!data.getUnit().dead) {
                     player.unit(data.getUnit());
