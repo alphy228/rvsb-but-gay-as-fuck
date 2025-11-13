8431eaf15b2176c8888725253312f2cbbb10c68d
alphy228 <89208763+alphy228@users.noreply.github.com>
2025-11-13 23:21:11 +0300

    test 1

--- src/main/java/net/voiddustry/redvsblue/player/Hud.java
+++ src/main/java/net/voiddustry/redvsblue/player/Hud.java
@@ -1,6 +1,6 @@
 package net.voiddustry.redvsblue.player;
 
-
+import java.lang.Runnable;
 import mindustry.content.Blocks;
 import mindustry.content.UnitTypes;
 import mindustry.game.Team;
@@ -18,8 +18,8 @@ import net.voiddustry.redvsblue.game.crux.CruxUnit;
 
 import static net.voiddustry.redvsblue.util.Utils.playerCount;
 
-public class Hud {
-    public static void update() {
+public class Hud implements Runnable{
+    public void run() {
 
         CruxUnit.checkUnitCount();
 
@@ -40,14 +40,12 @@ public class Hud {
 
         });
 
-        Groups.unit.each(u -> Call.label("[orange]X", 0.016F, u.x, u.y));
+        Groups.unit.each(u -> Call.label("[orange]X", 0.2F, u.x, u.y));
 
-
-        //allows mappers to edit 'redSpawns', only changes 'redSpawns',removes the objectiveFlag and the 100 team erekir liquid routers placed in order to send coordinates
-        //very cringe code
+        //trash
         if (Vars.state.rules.objectiveFlags.contains("updateRedSpawns")) {
             Vars.state.rules.objectiveFlags.remove("updateRedSpawns");
-            //hopefully no crash due to empty spawns
+            
             RedVsBluePlugin.redSpawns.each(spawnpoint -> {
                 if (spawnpoint != RedVsBluePlugin.redSpawns.firstOpt()) {
                     RedVsBluePlugin.redSpawns.remove(spawnpoint);
@@ -88,12 +86,6 @@ public class Hud {
                     if (data.getUnit().dead) {
                         data.setTeam(Team.crux);
                         player.team(data.getTeam());
-                    }
-                }
-    
-                if (!(player.unit() == null)) {
-                    if (player.unit().type == UnitTypes.quasar && player.unit().shield >= -10 && player.unit().shield <= 0) {
-                        player.unit().shield = 300;
                     }
                 }
                 
