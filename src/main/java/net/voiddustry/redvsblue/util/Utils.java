5694c03dd4d78f1fb6ea6f3181bb13f803445142
alphy228 <89208763+alphy228@users.noreply.github.com>
2025-11-30 17:51:58 +0300

    Update Utils.java

--- src/main/java/net/voiddustry/redvsblue/util/Utils.java
+++ src/main/java/net/voiddustry/redvsblue/util/Utils.java
@@ -103,7 +103,7 @@ public class Utils {
 
          Timer.schedule(() -> Groups.player.each(player -> {
             if (player.tileOn() != null && player.team() == Team.blue && player.unit() != null) {
-                if (player.tileOn().block() != null && (player.tileOn().block() == Blocks.cliff || (player.tileOn().block() instanceof Prop && player.tileOn().block().breakable) || player.tileOn().block instanceof TallBlock)) {
+                if (player.tileOn().block() != null && (player.tileOn().block() == Blocks.cliff || (player.tileOn().block() instanceof Prop && player.tileOn().block().breakable) || player.tileOn().block() instanceof TallBlock)) {
                     return;
                 }
                 if (player.tileOn().build != null && player.tileOn().build.team != Team.blue && player.tileOn().build.team != Team.derelict) {
