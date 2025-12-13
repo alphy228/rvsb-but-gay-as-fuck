b68b41b9aaac5ef91e13e477cdaaf2a48fe24794
alphy228 <89208763+alphy228@users.noreply.github.com>
2025-12-13 17:22:32 +0300

    Update RedVsBluePlugin.java

--- src/main/java/net/voiddustry/redvsblue/RedVsBluePlugin.java
+++ src/main/java/net/voiddustry/redvsblue/RedVsBluePlugin.java
@@ -460,9 +460,13 @@ public class RedVsBluePlugin extends Plugin {
 
         });
         
-        handler.<Player>register("pin-map","<filename>", "Admin only", (args, player) -> {
+        handler.<Player>register("pin-map","<filename>, <add/remove>", "Admin only", (args, player) -> {
             if (player.admin) {
+                if (args[1] == "add") {
                 MapVote.pinnedMaps.add(args[0]);
+                } else if (args[1] == "remove") {
+                MapVote.pinnedMaps.remove(args[0]);
+                }
             }
         });
 
