d6f9809e68c26a48fc040ace4979bd9fdeff1bf6
alphy228 <89208763+alphy228@users.noreply.github.com>
2026-01-15 08:27:42 +0300

    Update Evolution.java

--- src/main/java/net/voiddustry/redvsblue/evolution/Evolution.java
+++ src/main/java/net/voiddustry/redvsblue/evolution/Evolution.java
@@ -7,11 +7,11 @@ import static net.voiddustry.redvsblue.util.UnitsConfig.multp;
 
 public enum Evolution {
 
-    DAGGER(UnitTypes.dagger, 1, 2*multp, new String[] { "atrax", "mace", "elude", "flare", "nova", "mono" }, 1),
-    NOVA(UnitTypes.nova, 1, 3*multp, new String[] { "pulsar", "elude", "flare", "dagger", "mono" }), 1,
-    ELUDE(UnitTypes.elude, 1, 10*multp, new String[] { "atrax", "nova", "flare", "dagger", "mono" }, 1),
-    FLARE(UnitTypes.flare, 1, 6*multp, new String[] { "risso", "nova", "elude", "dagger", "mono" }, 1),
-    MONO(UnitTypes.mono, 1, 5*multp, new String[] { "poly", "nova", "elude", "dagger" }, 1),
+    DAGGER(UnitTypes.dagger, 1, 2*multp, new String[] { "atrax", "mace", "merui", "flare", "nova", "mono" }, 1),
+    NOVA(UnitTypes.nova, 1, 3*multp, new String[] { "pulsar", "merui", "flare", "dagger", "mono" }), 1,
+    MERUI(UnitTypes.merui, 1, 8*multp, new String[] { "atrax", "nova", "flare", "dagger", "mono" }, 1),
+    FLARE(UnitTypes.flare, 1, 6*multp, new String[] { "risso", "nova", "merui", "dagger", "mono" }, 1),
+    MONO(UnitTypes.mono, 1, 5*multp, new String[] { "poly", "nova", "merui", "dagger" }, 1),
 
     PULSAR(UnitTypes.pulsar, 2, 25*multp, new String[] { "quasar" }, 1),
     ATRAX(UnitTypes.atrax, 2, 18*multp, new String[] { "cleroi" }, 1),
@@ -40,7 +40,7 @@ public enum Evolution {
     CYERCE(UnitTypes.cyerce, 6, 150*multp, new String[] { "quad" }, 5),
 
     SCEPTER(UnitTypes.scepter, 7, 105*multp, new String[] { "vanquish" }, 6),
-    NAVANAX(UnitTypes.navanax, 7, 450*multp, new String[] { "flare", "elude", "dagger" }, , 9),
+    NAVANAX(UnitTypes.navanax, 7, 450*multp, new String[] { "flare", "merui", "dagger" }, , 9),
     TOXOPID(UnitTypes.toxopid, 7, 415*multp, new String[] { "collaris" }, 8),
     BRYDE(UnitTypes.bryde, 7, 180*multp, new String[] { "antumbra" }, 5),
     OBVIATE(UnitTypes.obviate, 7, 90*multp, new String[] { "quell" }, 5),
@@ -49,7 +49,7 @@ public enum Evolution {
     VANQUISH(UnitTypes.vanquish, 8, 250*multp, new String[] { "reign" }, 7),
     COLLARIS(UnitTypes.collaris, 8, 1170*multp, new String[] { "nova", "flare", "dagger", "mono" }, 7),
     ANTUMBRA(UnitTypes.antumbra, 8, 180*multp, new String[] { "eclipse" }, 6),
-    OCT(UnitTypes.oct, 8, 1500*multp, new String[] { "elude", "nova", "flare", "dagger" }, 9),
+    OCT(UnitTypes.oct, 8, 1500*multp, new String[] { "merui", "nova", "flare", "dagger" }, 9),
     QUELL(UnitTypes.quell, 8, 180*multp, new String[] { "disrupt" }, 6),
 
     REIGN(UnitTypes.reign, 9, 450*multp, new String[] { "conquer" }, 8),
@@ -57,10 +57,10 @@ public enum Evolution {
     DISRUPT(UnitTypes.disrupt, 9, 800*multp, new String[] { "nova", "flare", "dagger"}, 8),
 
 
-    CONQUER(UnitTypes.conquer, 10, 450*multp, new String[] { "elude", "nova", "flare", "mono" }, 8),
+    CONQUER(UnitTypes.conquer, 10, 450*multp, new String[] { "merui", "nova", "flare", "mono" }, 8),
     SEI(UnitTypes.sei, 10, 270*multp, new String[] { "omura" }, 8),
     
-    OMURA(UnitTypes.omura, 11, 1650*multp, new String[] { "elude", "nova", "dagger", "mono" }, 9);
+    OMURA(UnitTypes.omura, 11, 1650*multp, new String[] { "merui", "nova", "dagger", "mono" }, 9);
 
     public final UnitType unitType;
     public final int tier, cost;
