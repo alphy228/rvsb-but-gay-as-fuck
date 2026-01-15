2cbec19d9840b8851ea4165bb8446bed449d3149
alphy228 <89208763+alphy228@users.noreply.github.com>
2026-01-15 16:22:58 +0300

    Update Laboratory.java

--- src/main/java/net/voiddustry/redvsblue/game/stations/Laboratory.java
+++ src/main/java/net/voiddustry/redvsblue/game/stations/Laboratory.java
@@ -136,7 +136,7 @@ public class Laboratory {
         } else if (RedVsBluePlugin.stage > stage) {
             multiplier = 0.75f;
         } else {
-            multiplier = ((2^(stage-RedVsBluePlugin.stage-1))+1f);
+            multiplier = ((2^(stage-RedVsBluePlugin.stage-1)));
         }
         Log.info("stage for evolution "+evo+" - "+stage+" multiplier:"+multiplier);
         return multiplier;
