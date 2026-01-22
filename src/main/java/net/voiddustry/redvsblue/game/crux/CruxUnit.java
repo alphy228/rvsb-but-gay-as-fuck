74faeaf7b9468f4e636be5fd12470bdaf80ce3a0
alphy228 <89208763+alphy228@users.noreply.github.com>
2026-01-22 21:38:15 +0300

    Update CruxUnit.java

--- src/main/java/net/voiddustry/redvsblue/game/crux/CruxUnit.java
+++ src/main/java/net/voiddustry/redvsblue/game/crux/CruxUnit.java
@@ -160,7 +160,7 @@ public class CruxUnit {
                             }
                         }
                     });
-                    if (cruxUnits.get(type) < cruxUnitsCount/ClassChooseMenu.units.keys().toSeq().size()) {
+                    if (cruxUnits.get(type) < cruxUnitsCount/ClassChooseMenu.units.keys().toSeq().size) {
                         spawnCrux(type);
                     }
                 });
