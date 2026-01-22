0746b125b1e09c6493141f262604e4baad208b70
alphy228 <89208763+alphy228@users.noreply.github.com>
2026-01-22 14:54:44 +0300

    Delete src/main/java/net/voiddustry/redvsblue/game/building/BuildTicket.java

--- src/main/java/net/voiddustry/redvsblue/game/building/BuildTicket.java
+++
@@ -1,6 +0,0 @@
-package net.voiddustry.redvsblue.game.building;
-
-import mindustry.gen.Player;
-import mindustry.world.Block;
-
-public record BuildTicket(Player player, Block block, Boolean single, Boolean station, int cost) {}
