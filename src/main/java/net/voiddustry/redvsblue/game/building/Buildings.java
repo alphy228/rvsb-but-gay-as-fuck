9e17c011a51780b314ab8cfdb53018beb2ac2b80
alphy228 <89208763+alphy228@users.noreply.github.com>
2026-01-22 10:59:36 +0300

    Update Buildings.java

--- src/main/java/net/voiddustry/redvsblue/game/building/Buildings.java
+++ src/main/java/net/voiddustry/redvsblue/game/building/Buildings.java
@@ -13,14 +13,14 @@ public class Buildings {
     public static HashMap<Block, Integer> getPrices() {
         HashMap<Block, Integer> prices = new HashMap<>();
 
-        prices.put(Blocks.conveyor, 5);
-        prices.put(Blocks.router, 12);
-        prices.put(Blocks.junction, 20);
-        prices.put(Blocks.mechanicalDrill, 50);
-        prices.put(Blocks.graphitePress, 120);
-        prices.put(Blocks.duo, 75);
-        prices.put(Blocks.powerNode, 30);
-        prices.put(Blocks.battery, 60);
+        prices.put(Blocks.conveyor, 1);
+        prices.put(Blocks.router, 2);
+        prices.put(Blocks.junction, 2);
+        prices.put(Blocks.mechanicalDrill, 3);
+        prices.put(Blocks.graphitePress, 5);
+        prices.put(Blocks.duo, 5);
+        prices.put(Blocks.powerNode, 3);
+        prices.put(Blocks.battery, 6);
 
         return prices;
     }
