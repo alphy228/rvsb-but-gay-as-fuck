ea8f8ca31fd8a458c85a5453ecf97129deead4b6
alphy228 <89208763+alphy228@users.noreply.github.com>
2026-01-22 10:23:50 +0300

    Update UpdateConstructBlocks.java

--- src/main/java/net/voiddustry/redvsblue/game/building/UpdateConstructBlocks.java
+++ src/main/java/net/voiddustry/redvsblue/game/building/UpdateConstructBlocks.java
@@ -38,54 +38,30 @@ public class UpdateConstructBlocks implements Runnable {
   public void run() {
     try {
       
-      for (Building b : Groups.build) {
-        if (b.tile.block() instanceof ConstructBlock cb) {
-          ConstructBlock.ConstructBuild cbuild = (ConstructBlock.ConstructBuild)b;
-          Tile tile = cbuild.tile;
-          float x = cbuild.x;
-          float y = cbuild.y;
-          Block previous = cbuild.previous;
-          Block current = cbuild.current; 
-          float buildspeed = 0f;
-          int cost = prices.get(current);
-          //calculate build speed
-          Seq<Unit> buildingUnits = new Seq<>();
-          buildingUnits.clear();
-
-          Player player = null;
-          
-          for (Unit u : Groups.unit) {
-              if (u.buildPlan() != null) {
-                  if (u.buildPlan().worldContext) { 
-                      if (u.buildPlan().build() == b && u.isBuilding()) {
-                        
-                          if (u.getPlayer() != null) {
-                            player = u.getPlayer();
-                          }
-                        
-                          buildingUnits.add(u);
-                          if (u.buildPlan().breaking) {
-                              buildspeed = buildspeed - u.type.buildSpeed;
-                          } else {
-                              buildspeed = buildspeed + u.type.buildSpeed;
-                          }
-                      }
+      for (Unit u : Groups.unit) {
+        if (u.buildPlan() != null) {
+          if (u.buildPlan().worldContext) { 
+            if (u.isBuilding()) {
+              bp = u.buildPlan()
+                
+              int cost = prices.get(bp.block);
+                           
+              if (u.getPlayer() != null && u.buildPlan().breaking == false) {
+                Player player = u.getPlayer();
+                if (buildspeed > 0f) {
+                  Log.info("Attempting to consume " + cost+ " ,from player " + player);
+                  Unit builderUnit = buildingUnits.random();
+                  if (RedVsBluePlugin.players.get(player.uuid()).getScore()>=cost && (!(cost == null))) {
+                    RedVsBluePlugin.players.get(player.uuid()).subtractScore(cost);
+                    Log.info("finishing construction");
+                    Tile tile = Vars.world.tile(bp.x,bp.y);
+                    Call.constructFinish(tile,bp.block,null,(byte)bp.rotation,player.team,bp.config);
+                    tile.build.placed();
+                  } else {
+                    Vars.world.tile((int)b.x,(int)b.y).setNet(Blocks.air,Team.derelict,0);
                   }
+                }
               }
-          }
-
-          if (buildspeed > 0f) {
-            Log.info("Attempting to consume " + cost+ " ,from player " + player);
-            Unit builderUnit = buildingUnits.random();
-            if (RedVsBluePlugin.players.get(player.uuid()).getScore()>=cost) {
-              RedVsBluePlugin.players.get(player.uuid()).subtractScore(cost);
-              int remainingItemCost2=0;
-              Log.info("finishing construction");
-              cbuild.progress=1.0001f;
-              Call.constructFinish(tile,current,null,(byte)builderUnit.buildPlan().rotation,b.team,builderUnit.buildPlan().block.instantBuild ? builderUnit.buildPlan().config : null);
-              tile.build.placed();
-            } else {
-              Vars.world.tile((int)b.x,(int)b.y).setNet(Blocks.air,Team.derelict,0);
             }
           }
         }
