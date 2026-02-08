e1dae352dc86b0eeabda63b26aabe0a8acf91860
alphy228 <89208763+alphy228@users.noreply.github.com>
2026-02-08 10:02:06 +0300

    Update ArmorWorkbench.java

--- src/main/java/net/voiddustry/redvsblue/game/stations/ArmorWorkbench.java
+++ src/main/java/net/voiddustry/redvsblue/game/stations/ArmorWorkbench.java
@@ -90,6 +90,7 @@ public class ArmorWorkbench {
     }
 
     public static void buyWorkbench(Player player, Tile tile) {
+        Log.info("wrokbench");
         if (players.get(player.uuid()).getScore() < 8) {
             player.sendMessage(Bundle.format("station.not-enough-money", Bundle.findLocale(player.locale), 8));
         } else {
